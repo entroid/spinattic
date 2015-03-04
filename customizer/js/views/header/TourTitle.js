e9e4208cc7ab4f5845087e1d09400c63ac1125b6
Maxi <hernanh@Hernan-Hs-Mac-mini.local>
2015-03-04 13:59:52 -0300

    nueva vista del header

---
+++ customizer/js/views/header/TourTitle.js
@@ -0,0 +1,29 @@
+define([
+  'jquery',
+  'underscore',
+  'backbone',
+  'text!templates/header/tourtitle.html'
+
+], function($, _, Backbone, tourtitle){
+
+  var TourTitle = Backbone.View.extend({
+    el: $(".header-side"),
+    initialize: function () {
+
+      
+    },
+    events:{
+    
+         },
+    render: function(){
+    
+      var compiledTemplate = _.template(tourtitle);
+      $(this.el).append( compiledTemplate ); 
+   
+    }    
+    
+  });
+
+  return TourTitle;
+  
+});
