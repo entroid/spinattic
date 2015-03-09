0d29b717b323beeffb4f4874175a044322cc99ad
Maxi <hernanh@Hernan-Hs-Mac-mini.local>
2015-03-09 17:36:08 -0300

    agregado de editor de Skills

--- customizer/js/views/sidebar/SkinCustomizerItem.js
+++ customizer/js/views/sidebar/SkinCustomizerItem.js
@@ -2,18 +2,35 @@ define([
 	'jquery',
 	'underscore',
 	'backbone',
-	'text!templates/sidebar/skincustomizeritem.html'
+	'text!templates/sidebar/skincustomizeritem.html',
+	'views/modal/SkillEditor',
 
-], function($, _, Backbone,skincustomizeritem){
+], function($, _, Backbone,skincustomizeritem,SkillEditor){
 
 	var SkinCustomizerItem = Backbone.View.extend({
+
+		events:{
+		},
 		
 		render:function(){
 			
-			data = this.model.get("data");
-			console.log(data)
+			var data = this.model.get("data");
 			var compiledTemplate = _.template(skincustomizeritem,{data:data});
 			$("#skinCustomizer-menu .skill-list").append(compiledTemplate);
+
+			$('#skill-' + data.id ).data("skill",data);
+			$('#skill-' + data.id + ' .customizelink').click(this.editSkill);
+		  	
+		},
+
+		editSkill:function(e){
+			var skill = $(e.target).parent("li").data("skill");
+			console.log(skill)
+			var SkillModel = Backbone.Model.extend({});
+			skillModel = new SkillModel({data:skill});
+			var skillEditor = new SkillEditor({model:skillModel});
+			skillEditor.render("skillsEditor",skillEditor.renderExtend);
+		
 		}
 		
 	});
