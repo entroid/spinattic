19d4436df6ca967101bd5bf3b4b945ad0fa76bc1
hernán <hernansahha@gmail.com>
2015-03-12 18:26:19 -0300

    general settings dropdown fixes

--- customizer/js/views/sidebar/VirtualTourSettingsMenuView.js
+++ customizer/js/views/sidebar/VirtualTourSettingsMenuView.js
@@ -1,100 +1,108 @@
 define([
-	'jquery',
-	'underscore',
-	'backbone',
-	'views/sidebar/SidebarSubMenu',
-	'text!templates/sidebar/virtualTourSettingsMenu.html',
-	'mCustomScrollbar',
-	'helpers/HelpFunctions',
-	'views/sidebar/MapView',
-	'helpers/ManageData',
-	
+    'jquery',
+    'underscore',
+    'backbone',
+    'views/sidebar/SidebarSubMenu',
+    'text!templates/sidebar/virtualTourSettingsMenu.html',
+    'mCustomScrollbar',
+    'helpers/HelpFunctions',
+    'views/sidebar/MapView',
+    'helpers/ManageData',
+    
 ], function($, _, Backbone,SidebarSubMenu,virtualTourSettingsMenu,mCustomScrollbar,HelpFunctions,MapView,ManageData){
 
-	var VirtualTourSettingsMenuView = SidebarSubMenu.extend({
+    var VirtualTourSettingsMenuView = SidebarSubMenu.extend({
 
-		initialize: function () {
-		  this.events = this.events || {};
-		  var eventKey = 'click #' + this.model.get("elem") + ' h3';
-		  this.events[eventKey] = 'openSubItems';
-		  var checkboxesEvent = 'click #'+ this.model.get("elem") +' .checkboxes li';
-		  this.events[checkboxesEvent] = 'selectCheckboxes';
-		  var inputs = "keyup #virtualTourSettings-menu .tour-data";
-		  var inputsChange = "mouseup #virtualTourSettings-menu .tour-data";
-		  this.events[inputs] = 'insertData';
-		  this.events[inputsChange] = 'insertData';
-		  this.delegateEvents();
-		},
+        initialize: function () {
+          this.events = this.events || {};
+          var eventKey = 'click #' + this.model.get("elem") + ' h3';
+          this.events[eventKey] = 'openSubItems';
+          var checkboxesEvent = 'click #'+ this.model.get("elem") +' .checkboxes li';
+          this.events[checkboxesEvent] = 'selectCheckboxes';
+          var inputs = "keyup #virtualTourSettings-menu .tour-data";
+          var inputsChange = "mouseup #virtualTourSettings-menu .tour-data";
+          this.events[inputs] = 'insertData';
+          this.events[inputsChange] = 'insertData';
+          this.delegateEvents();
+        },
 
-		render: function(){
-			var data = {
+        render: function(){
+            var data = {
 
-				settings:tourData.krpano.settings,
-				autorotate:tourData.krpano.autorotate,
-				control:tourData.krpano.control,
-			}
-			var compiledTemplate = _.template(virtualTourSettingsMenu,{data:data});
-			$(this.el).append( compiledTemplate ); 
-			var elem = this.model.get("elem");
-			this.$elem = $("#"+elem);
-			
-			this.model.set("elemWidth",this.$elem.width());
-			
-			var helpFunctions = new HelpFunctions();
+                settings:tourData.krpano.settings,
+                autorotate:tourData.krpano.autorotate,
+                control:tourData.krpano.control,
+            }
+            var compiledTemplate = _.template(virtualTourSettingsMenu,{data:data});
+            $(this.el).append( compiledTemplate ); 
+            var elem = this.model.get("elem");
+            this.$elem = $("#"+elem);
+            
+            this.model.set("elemWidth",this.$elem.width());
+            
+            var helpFunctions = new HelpFunctions();
 
-			helpFunctions.setInnerHeight(elem);
-			$(window).resize(function(){
-					helpFunctions.setInnerHeight(elem);
+            helpFunctions.setInnerHeight(elem);
+            $(window).resize(function(){
+                    helpFunctions.setInnerHeight(elem);
 
-			});
-			$("#"+elem+" .inner").mCustomScrollbar({
-				theme:"minimal-dark",
-				scrollInertia:300,
-				});
+            });
+            $("#"+elem+" .inner").mCustomScrollbar({
+                theme:"minimal-dark",
+                scrollInertia:300,
+                });
 
-			this.show();
-			this.mapView = new MapView();
-			this.mapView.render(elem);
-			
-			helpFunctions.dropDown(".dd-settingmenu");
-			helpFunctions.checkbox(".check-group","fa-check-square","fa-square");
-			helpFunctions.selectChoice(".checkboxes li","fa-circle-o","fa-circle");
-			helpFunctions.toolTip(".submenu .help","aside help");
-		},
+            this.show();
+            this.mapView = new MapView();
+            this.mapView.render(elem);
+            
+            helpFunctions.dropDown(".dd-settingmenu");
+            helpFunctions.checkbox(".check-group","fa-check-square","fa-square");
+            helpFunctions.selectChoice(".checkboxes li","fa-circle-o","fa-circle");
+            helpFunctions.toolTip(".submenu .help","aside help");
+        },
 
-		openSubItems:function(e){
-			$mineSub = $(e.target).parent("li").find(".sub-items")
-			$("#"+this.model.get("elem")+" .sub-items").not($mineSub).slideUp();
-			$mineSub.slideToggle();
-			//google.maps.event.trigger(map, "resize");
-			this.mapView.refreshSizeMap()
-		},
+        openSubItems:function(e){
 
-		selectCheckboxes:function(e){
-			var target = $(e.target);
-			if(target.is("span")){
-				elem = $(e.target).parent("li")
-			}else{
-				elem = $(e.target)
+            var clickParent = $(e.target).parents("li"),
+            caret = clickParent.find('.caret'),
+            $mineSub = clickParent.find(".sub-items");
 
-			}
-			if($(elem).data("evt") == "singleLoc"){
-				$("#virtualTourSettings-menu .map-wrapper").show()
-			}else{
-				$("#virtualTourSettings-menu .map-wrapper").hide()
-			}
-		},
+            $("#" + this.model.get("elem") + " .sub-items").not( $mineSub ).slideUp();
+            clickParent.siblings().find('.caret').removeClass( 'fa-caret-up').addClass( 'fa-caret-down');
 
-		insertData:function(e){
+            caret.toggleClass('fa-caret-down fa-caret-up');
+            $mineSub.slideToggle();
 
-			console.log(e)
-			var manageData = new ManageData();
-			manageData.saveSettings(e);
-		
-		}
-		
-	});
+            //google.maps.event.trigger(map, "resize");
+            this.mapView.refreshSizeMap()
+        },
 
-	return VirtualTourSettingsMenuView;
-	
+        selectCheckboxes:function(e){
+            var target = $(e.target);
+            if(target.is("span")){
+                elem = $(e.target).parent("li")
+            }else{
+                elem = $(e.target)
+
+            }
+            if($(elem).data("evt") == "singleLoc"){
+                $("#virtualTourSettings-menu .map-wrapper").show()
+            }else{
+                $("#virtualTourSettings-menu .map-wrapper").hide()
+            }
+        },
+
+        insertData:function(e){
+
+            console.log(e)
+            var manageData = new ManageData();
+            manageData.saveSettings(e);
+        
+        }
+        
+    });
+
+    return VirtualTourSettingsMenuView;
+    
 });
