be165bbb7a0f0606752f2de5f086c6852b3549b2
entroid <hernansahha@gmail.com>
2015-04-21 20:09:55 -0300

    several fixes

--- customizer/js/views/footer/SceneMenuView.js
+++ customizer/js/views/footer/SceneMenuView.js
@@ -1,125 +1,135 @@
 define([
-	'jquery',
-	'underscore',
-	'backbone',
-	'x2js',
-	'jqueryui',
-	'text!templates/footer/sceneMenu.html',
-	'helpers/HelpFunctions',
-	'mCustomScrollbar',
-	'views/main/TourView',
-	'models/main/TourModel',
-	'views/sidebar/SceneSettingsMenuView',
-	'views/sidebar/ViewSettingsMenuView',
-	'helpers/ManageData',
-	'helpers/ManageHotSpots'
+    'jquery',
+    'underscore',
+    'backbone',
+    'x2js',
+    'jqueryui',
+    'text!templates/footer/sceneMenu.html',
+    'helpers/HelpFunctions',
+    'mCustomScrollbar',
+    'views/main/TourView',
+    'models/main/TourModel',
+    'views/sidebar/SceneSettingsMenuView',
+    'views/sidebar/ViewSettingsMenuView',
+    'helpers/ManageData',
+    'helpers/ManageHotSpots'
 
 ], function($, _, Backbone,x2js,jqueryui, bottomMenu,HelpFunctions,mCustomScrollbar,TourView,TourModel,SceneSettingsMenuView,ViewSettingsMenuView,ManageData,ManageHotSpots){
 
-	var SceneMenuView = Backbone.View.extend({
-		el: $("footer.main-footer"),
-		mycollection:[],
-		initialize: function () {
-				_.bindAll(this,"openScene")
-			
-		},
-		events:{
-			"click #sceneMenu .fa-close":"removeItem",
-			"click #sceneMenu li":"openScene"     
-				 },
-		render: function(){
-			if(this.collection){
-				var jsonObj = this.collection.toJSON();
-				this.mycollection = this.collection.toJSON();
-			}else{
-				var jsonObj = [];
-			}
+    var SceneMenuView = Backbone.View.extend({
+        el: $("footer.main-footer"),
+        mycollection:[],
+        initialize: function () {
+                _.bindAll(this,"openScene")
+            
+        },
+        events:{
+            "click #sceneMenu .fa-close":"removeItem",
+            "click #sceneMenu li":"openScene"     
+                 },
+        render: function(){
+            if(this.collection){
+                var jsonObj = this.collection.toJSON();
+                this.mycollection = this.collection.toJSON();
+            }else{
+                var jsonObj = [];
+            }
 
-			if($(".main-footer .scene-wrapper").length){
-				$(".main-footer .scene-wrapper").remove();
-			}
-			var compiledTemplate = _.template(bottomMenu,{jsonObj:jsonObj});
-			$(this.el).append( compiledTemplate ); 
- 			
-			_.each(jsonObj,function(val,ind){
-				$("#sceneMenu li:eq("+ind+")").data("scene",val);
-				if(val.hotspot){
-					$("#sceneMenu li:eq("+ind+")").data("hotspots",val.hotspot)
-				}
-			})
+            if($(".main-footer .scene-wrapper").length){
+                $(".main-footer .scene-wrapper").remove();
+            }
+            var compiledTemplate = _.template(bottomMenu,{jsonObj:jsonObj});
+            $(this.el).append( compiledTemplate ); 
+            
+            _.each(jsonObj,function(val,ind){
+                $("#sceneMenu li:eq("+ind+")").data("scene",val);
+                if(val.hotspot){
+                    $("#sceneMenu li:eq("+ind+")").data("hotspots",val.hotspot)
+                }
+            })
 
-			var helpFunctions = new HelpFunctions();
-			helpFunctions.toolTip("#sceneMenu li img","footer");
-		
-			$("#sceneMenu").sortable({
-				beforeStop:function(evt,ui){
+            var helpFunctions = new HelpFunctions();
+            helpFunctions.toolTip("#sceneMenu li img","footer");
+        
+            $("#sceneMenu").sortable({
+                beforeStop:function(evt,ui){
 
-					var manageData = new ManageData();
-					manageData.SaveNewSceneOrder()
+                    var manageData = new ManageData();
+                    manageData.SaveNewSceneOrder()
 
-				}
-			});
+                }
+            });
 
-			liwidth = $("#sceneMenu li").outerWidth();
-			liright = $("#sceneMenu li").css("margin-right");
-			liright = parseInt(liright.replace("px",""));
-			liall = liwidth+liright;
-			allwidth = liall * $("#sceneMenu li").length;
-			$("#sceneMenu").width(allwidth+20);
+            liwidth = $("#sceneMenu li").outerWidth();
+            liright = $("#sceneMenu li").css("margin-right");
+            liright = parseInt(liright.replace("px",""));
+            liall = liwidth+liright;
+            allwidth = liall * $("#sceneMenu li").length;
+            $("#sceneMenu").width(allwidth+20);
 
-			$(".scene-wrapper").mCustomScrollbar({
-							theme:"light",
-							scrollInertia:300,
-							horizontalScroll: true,
-					});
-		},
+            $(".scene-wrapper").mCustomScrollbar({
+                            theme:"light",
+                            scrollInertia:300,
+                            horizontalScroll: true,
+                    });
+        },
 
-		removeItem:function(e){
-			$(e.target).parent().fadeOut(function(){
-				var thisname = $(this).data("scene")._scene_id;
-				this.remove();
+        removeItem:function(e){
+            $(e.target).parent().fadeOut(function(){
+                var thisname = $(this).data("scene")._scene_id;
+                this.remove();
 
-				if($("#tour").data("scene")._scene_id == thisname){
-					$("#sceneMenu li:eq(0) img").trigger("click");
-				}
+                if($("#tour").data("scene")._scene_id == thisname){
+                    $("#sceneMenu li:eq(0) img").trigger("click");
+                }
 
-				var manageData = new ManageData();
-				manageData.SaveNewSceneOrder()
-			})
-		},
+                var manageData = new ManageData();
+                manageData.SaveNewSceneOrder()
+            })
+        },
 
-		openScene:function(e){
+        openScene:function(e){
 
-			var helpFunctions = new HelpFunctions();
+            var helpFunctions = new HelpFunctions();
 
-			$thisli = $(e.target).parent();
-			$("#tour").data("scene",$thisli.data("scene"))
-			helpFunctions.refreshData();
-			var customparam = jQuery.extend({},$thisli.data("scene"));
-			delete customparam.view._segment;
-			delete customparam.preview._segment;
-			delete customparam.image._segment;
-			delete customparam._segment;
-			delete customparam.hotspot;
-			var krpano = document.getElementById("krpanoSWFObject");
-			var param = helpFunctions.mapJSONToUriParams(customparam);
-			param = param.replace(/:_/g,".");
-			krpano.call("loadscene('"+$thisli.attr("id")+"','"+param+"');");
-			if($thisli.data("hotspots")){
+            $thisli = $(e.target).parent();
+            $("#tour").data("scene",$thisli.data("scene"))
+            helpFunctions.refreshData();
+            var customparam = jQuery.extend({},$thisli.data("scene"));
+            delete customparam.view._segment;
+            delete customparam.preview._segment;
+            delete customparam.image._segment;
+            delete customparam._segment;
+            delete customparam.hotspot;
+            var krpano = document.getElementById("krpanoSWFObject");
+            var param = helpFunctions.mapJSONToUriParams(customparam);
+            param = param.replace(/:_/g,".");
+            krpano.call("loadscene('"+$thisli.attr("id")+"','"+param+"');");
+            if($thisli.data("hotspots")){
 
-				var manageHotSpots = new ManageHotSpots();
-				krpano.set("events.onpreviewcomplete","js(initHotSpots())");
-				krpano.set("events.keep",true);
-			}
-			$("#sceneMenu li").removeClass("selected");
-			$thisli.addClass("selected")
+                var manageHotSpots = new ManageHotSpots();
+                krpano.set("events.onpreviewcomplete","js(initHotSpots())");
+                krpano.set("events.keep",true);
+            }
+            $("#sceneMenu li").removeClass("selected");
+            $thisli.addClass("selected")
 
-		$(".hotspotwindow .save-and-close").trigger("click");
+            $(".hotspotwindow .save-and-close").trigger("click");
 
-		}
-		
-	});
+            this.openAsideMenu();
 
-	return SceneMenuView;
-	
+        },
+
+        openAsideMenu: function() {
+            var btn = $('aside #sceneSettings');
+
+            if( !$(btn).hasClass('selected') ) {
+                $(btn).click();
+            }
+        }
+        
+    });
+
+    return SceneMenuView;
+    
 });
--- customizer/js/views/sidebar/HotSpotsMenuView.js
+++ customizer/js/views/sidebar/HotSpotsMenuView.js
@@ -1,83 +1,83 @@
 define([
-	'jquery',
-	'underscore',
-	'backbone',
-	'views/sidebar/SidebarSubMenu',
-	'text!templates/sidebar/hotspotsMenu.html',
-	'helpers/ManageHotSpots',
-	'models/main/HotSpotWindowModel',
-	'helpers/ManageData',
-	'helpers/HelpFunctions',
-	'views/modal/HotSpotStyleEditor'
+    'jquery',
+    'underscore',
+    'backbone',
+    'views/sidebar/SidebarSubMenu',
+    'text!templates/sidebar/hotspotsMenu.html',
+    'helpers/ManageHotSpots',
+    'models/main/HotSpotWindowModel',
+    'helpers/ManageData',
+    'helpers/HelpFunctions',
+    'views/modal/HotSpotStyleEditor'
 
 
 ], function($, _, Backbone,SidebarSubMenu,hotspotsMenu,ManageHotSpots,HotSpotWindowModel,ManageData,HelpFunctions,HotSpotStyleEditor){
 
-	var HotSpotsMenuView = SidebarSubMenu.extend({
-		hotspotCount: 0,
-		selectedset:"",
-		events:{
-			"click #hotSpots-menu li.htpt": "addHotSpot",
-			"click #hotspot-styles .selector":"selectStyleClick",
-			"click #open-styles":"showhideStyles",
-			"click #hotspot-styles .add-link": "showHotspotsStyleEditor"
-				 },
-		hotspotstyleeditor:null,
-		
-		render: function(){
-			var styles = tourData.krpano.style;
-			var compiledTemplate = _.template(hotspotsMenu,{styles:styles});
-			$(this.el).append( compiledTemplate ); 
-			var helpFunctions = new HelpFunctions();
-			helpFunctions.selectChoice("#hotspot-styles .selector","fa-circle-o","fa-circle");
-			this.$elem = $("#"+this.model.get("elem"));
-			this.model.set("elemWidth",this.$elem.width());
-			this.selectStyle("set1");
-			this.show();
-		},
+    var HotSpotsMenuView = SidebarSubMenu.extend({
+        hotspotCount: 0,
+        selectedset:"",
+        events:{
+            "click #hotSpots-menu li.htpt": "addHotSpot",
+            "click #hotspot-styles .selector":"selectStyleClick",
+            "click #open-styles":"showhideStyles",
+            "click #hotspot-styles .add-link": "showHotspotsStyleEditor"
+                 },
+        hotspotstyleeditor:null,
+        
+        render: function(){
+            var styles = tourData.krpano.style;
+            console.log(tourData)
+            var compiledTemplate = _.template(hotspotsMenu,{styles:styles});
+            $(this.el).append( compiledTemplate ); 
+            var helpFunctions = new HelpFunctions();
+            helpFunctions.selectChoice("#hotspot-styles .selector","fa-circle-o","fa-circle");
+            this.$elem = $("#"+this.model.get("elem"));
+            this.model.set("elemWidth",this.$elem.width());
+            this.selectStyle("set1");
+            this.show();
+        },
 
-		addHotSpot:function(e){
-			if($("#tour").data("scene").hotspot){
-				var numbs = []
-				_.each($("#tour").data("scene").hotspot,function(hp,val){
-					var num = parseInt(hp._name.replace("spot",""));
-					numbs.push(num);
-				})
+        addHotSpot:function(e){
+            if($("#tour").data("scene").hotspot){
+                var numbs = []
+                _.each($("#tour").data("scene").hotspot,function(hp,val){
+                    var num = parseInt(hp._name.replace("spot",""));
+                    numbs.push(num);
+                })
 
 
-				this.hotspotCount = Math.max.apply(Math, numbs); 
-				console.log(this.hotspotCount)
-			}else{
-				this.hotspotCount = 0;
-			}
-			this.hotspotCount++;
-			var name = $(e.target).prop("tagName")
-			if(name == "DIV"){
-			var myid = $(e.target).parents("li").attr("id");
-			}else{
-			var myid = $(e.target).attr("id");
-			}
-			 switch(myid){
-				 case "link":
-				urlid = "5"
-				 break;
-				 case "video":
-					urlid = "4"
-				 break;
-				 case "photo":
-					urlid = "3"
-				 break;
-				 case "info":
-					urlid = "2"
-				 break;
-				 case "arrow":
-				   urlid = "1"
-				 break;
-			 }
-			var me = this;
+                this.hotspotCount = Math.max.apply(Math, numbs);
+            }else{
+                this.hotspotCount = 0;
+            }
+            this.hotspotCount++;
+            var name = $(e.target).prop("tagName")
+            if(name == "DIV"){
+            var myid = $(e.target).parents("li").attr("id");
+            }else{
+            var myid = $(e.target).attr("id");
+            }
+             switch(myid){
+                 case "video":
+                    urlid = "5"
+                 break;
+                 case "photo":
+                    urlid = "4"
+                 break;
+                 case "link":
+                    urlid = "3"
+                 break;
+                 case "info":
+                    urlid = "2"
+                 break;
+                 case "arrow":
+                   urlid = "1"
+                 break;
+             }
+            var me = this;
 
-			$.ajax({
-				url:"data/xml.php?t=htspts&c=1&id="+urlid,
+            $.ajax({
+                url:"data/xml.php?t=htspts&c=1&id="+urlid,
                  dataType: "html",
                  success:function(data){
 
@@ -104,52 +104,53 @@ define([
                     openHotspotWindowEditor(newHotspot._name);
                  }
              })
-		},
-		selectStyleClick:function(ev){
-			var obj = ev.currentTarget
-			var set = $(obj).next().find(".rowinrow").data("family");
-			this.selectStyle(set)
-		},
+        },
+        selectStyleClick:function(ev){
+            var obj = ev.currentTarget
+            var set = $(obj).next().find(".rowinrow").data("family");
+            this.selectStyle(set)
+        },
 
-		selectStyle:function(set){                
-			var styles = tourData.krpano.style;
-			var selected = [];
-			_.each(styles,function(elem,ind){
-				var name = elem._name;
-				name = name.split("_");
-				
-				if(name[1] == set){
-					
-					selected.push(elem)
-				}
-			});
+        selectStyle:function(set){                
+            var styles = tourData.krpano.style;
+            var selected = [];
+            _.each(styles,function(elem,ind){
+                var name = elem._name;
+                name = name.split("_");
+                
+                if(name[1] == set){
+                    
+                    selected.push(elem)
+                }
+            });
 
-			this.selectedset = set;
-			$("#hotspots-menu-header").html("").append('<ul></ul>');
+            this.selectedset = set;
+            $("#hotspots-menu-header").html("").append('<ul></ul>');
 
-			_.each(selected,function(elem,ind){ 
-				var crop = elem._crop.split("|")
-				$("#hotspots-menu-header ul").append(' <li id="'+elem._kind+'" class="htpt"><div class="selected icons"><div style="background-image:url(data/'+elem._url+');background-position:-'+crop[0]+'px 0"></div></div></li>');                    
-			})
-				
-			$("#hotspots-menu-header").append('<div id="open-styles">hotspots styles <div class="arrow"></div>');
-		},
+            _.each(selected,function(elem,ind){ 
+                var crop = elem._crop.split("|")
+                console.log(crop)
+                $("#hotspots-menu-header ul").append(' <li id="'+elem._kind+'" class="htpt"><div class="selected icons"><div style="background-image:url(data/'+elem._url+');background-position:-'+crop[0]+'px 0"></div></div></li>');                    
+            })
+                
+            $("#hotspots-menu-header").append('<div id="open-styles">hotspots styles <div class="arrow"></div>');
+        },
 
-		showhideStyles:function(){
-			$("#hotspot-styles").slideToggle()
-		},
+        showhideStyles:function(){
+            $("#hotspot-styles").slideToggle()
+        },
 
-		showHotspotsStyleEditor: function () {
-			if($(".pano-manager").length) {            
-				this.hotspotstyleeditor.removeView();                
-			}else{
-				this.hotspotstyleeditor = new HotSpotStyleEditor();
-				this.hotspotstyleeditor.render("hotspotStyleEditor",this.hotspotstyleeditor.renderExtend);
-			}
-		}
-		
-	});
+        showHotspotsStyleEditor: function () {
+            if($(".pano-manager").length) {            
+                this.hotspotstyleeditor.removeView();                
+            }else{
+                this.hotspotstyleeditor = new HotSpotStyleEditor();
+                this.hotspotstyleeditor.render("hotspotStyleEditor",this.hotspotstyleeditor.renderExtend);
+            }
+        }
+        
+    });
 
-	return HotSpotsMenuView;
-	
+    return HotSpotsMenuView;
+    
 });
