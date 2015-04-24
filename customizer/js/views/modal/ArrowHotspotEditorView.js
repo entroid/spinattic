e4f6309904dd234450dacfe0dba3dac1e571ca4d
hernan <hernan.ainsa@globant.com>
2015-04-24 15:52:49 -0300

    hotspot arrow select scene dropdown fix

--- customizer/js/views/modal/ArrowHotspotEditorView.js
+++ customizer/js/views/modal/ArrowHotspotEditorView.js
@@ -1,198 +1,202 @@
 define([
-	'jquery',
-	'underscore',
-	'backbone',
-	'views/modal/Modal',
-	'text!templates/modal/hotspotarrow.html',
-	'helpers/HelpFunctions',
-	'views/modal/HotSpotsDropDown',
-	'helpers/ManageData',
+    'jquery',
+    'underscore',
+    'backbone',
+    'views/modal/Modal',
+    'text!templates/modal/hotspotarrow.html',
+    'helpers/HelpFunctions',
+    'views/modal/HotSpotsDropDown',
+    'helpers/ManageData',
 
 
 ], function($, _, Backbone,Modal,hotspotarrow,HelpFunctions,HotSpotsDropDown,ManageData){
 
-	var ArrowHotspotEditorView = Modal.extend({
-		
-		initialize: function () {
-		_.bindAll(this);        
-		 _.extend(this.events, Modal.prototype.events);
-		},
-		events:{
+    var ArrowHotspotEditorView = Modal.extend({
+        
+        initialize: function () {
+        _.bindAll(this);        
+         _.extend(this.events, Modal.prototype.events);
+        },
+        events:{
 
-				 },
-		
-		renderExtend:function(){
-			var manageData = new ManageData();
-			var num = this.myid.replace("spot","");
-			$("#"+this.myid).addClass("arrow-hotspot");
-			$("#"+this.myid+" header h2").text("Arrow Hotspot. ID "+num+":")
+                 },
+        
+        renderExtend:function(){
+            var manageData = new ManageData();
+            var num = this.myid.replace("spot","");
+            $("#"+this.myid).addClass("arrow-hotspot");
+            $("#"+this.myid+" header h2").text("Arrow Hotspot. ID "+num+":")
 
-			var allData = this.model.get("allData");
-			console.log(allData)
-			var scenes = tourData.krpano.scene;			
-			var compiledTemplate = _.template(hotspotarrow,{num:num,allData:allData,scenes:scenes,num:num})
-			$("#"+this.myid+" .inner-modal").html(compiledTemplate);
-			var me = this;
-			$("#"+me.myid).find(".fa-close").remove();
-			$("#"+me.myid+" header .save-and-close").unbind("click")
+            var allData = this.model.get("allData");
+            console.log(allData)
+            var scenes = tourData.krpano.scene;         
+            var compiledTemplate = _.template(hotspotarrow,{num:num,allData:allData,scenes:scenes,num:num})
+            $("#"+this.myid+" .inner-modal").html(compiledTemplate);
+            var me = this;
+            $("#"+me.myid).find(".fa-close").remove();
+            $("#"+me.myid+" header .save-and-close").unbind("click")
 
-			$("#"+me.myid+" header .save-and-close").click(function(){
-				
-				var selectedscene = $("#"+me.myid+" h3 .selectedScene").text();
-				var hotspot = allData;
-				if(selectedscene == "none"){
-					selectedscene = "";
-				}
-				hotspot._rotate = $("#"+me.myid+" .rotate").val()
-				hotspot._linkedscene = selectedscene;
-				manageData.changeDataInHotSpot($("#tour").data("scene")._name, hotspot)
-				
-				$(this).parents(".modal").fadeOut(function(){
-					me.removeThis();
-				})
-			})
+            $("#"+me.myid+" header .save-and-close").click(function(){
+                
+                var selectedscene = $("#"+me.myid+" h3 .selectedScene").text();
+                var hotspot = allData;
+                if(selectedscene == "none"){
+                    selectedscene = "";
+                }
+                hotspot._rotate = $("#"+me.myid+" .rotate").val()
+                hotspot._linkedscene = selectedscene;
+                manageData.changeDataInHotSpot($("#tour").data("scene")._name, hotspot)
+                
+                $(this).parents(".modal").fadeOut(function(){
+                    me.removeThis();
+                })
+            })
 
-			$("#"+me.myid+" .rotate").change(function(){
-				var val = $("#"+me.myid+" .rotate").val();
-				var hpdata = $("#"+me.myid).data("spotdata");
-				var krpano = document.getElementById("krpanoSWFObject");
-				krpano.set("hotspot["+hpdata._name+"].rotate",val);
-			})
-			
-			var $me = $("#"+this.myid);
-			var oldWidth = $me.width();
+            $("#"+me.myid+" .rotate").change(function(){
+                var val = $("#"+me.myid+" .rotate").val();
+                var hpdata = $("#"+me.myid).data("spotdata");
+                var krpano = document.getElementById("krpanoSWFObject");
+                krpano.set("hotspot["+hpdata._name+"].rotate",val);
+            })
+            
+            var $me = $("#"+this.myid);
+            var oldWidth = $me.width();
 
-			var spotName =  this.myid;
-			$me.find(".removeHotspot").click(function(){
-				var krpano = document.getElementById("krpanoSWFObject");
-				krpano.call("removehotspot("+spotName+")");
-				manageData.removeHotSpot($("#tour").data("scene")._name, spotName);
-				$("#"+myid+" header .save-and-close").trigger("click");
-			})
+            var spotName =  this.myid;
+            $me.find(".removeHotspot").click(function(){
+                var krpano = document.getElementById("krpanoSWFObject");
+                krpano.call("removehotspot("+spotName+")");
+                manageData.removeHotSpot($("#tour").data("scene")._name, spotName);
+                $("#"+myid+" header .save-and-close").trigger("click");
+            })
 
-			$me.find("#onoffswitchhparrow-"+num).click(function(){
-				if($(this).is(":checked")){
-					var hpdata = $me.data("spotdata");
-					var selectedscene = $("#"+me.myid+" h3 .selectedScene").text();
-					var krpano = document.getElementById("krpanoSWFObject");
-					krpano.call("addhotspot("+hpdata._name +")");
-					window.selectScene = function(selectedscene){
-						setTimeout(function(){
-							$("#"+selectedscene+" img").trigger("click")
-						},500)
-					}
+            $me.find("#onoffswitchhparrow-"+num).click(function(){
+                if($(this).is(":checked")){
+                    var hpdata = $me.data("spotdata");
+                    var selectedscene = $("#"+me.myid+" h3 .selectedScene").text();
+                    var krpano = document.getElementById("krpanoSWFObject");
+                    krpano.call("addhotspot("+hpdata._name +")");
+                    window.selectScene = function(selectedscene){
+                        setTimeout(function(){
+                            $("#"+selectedscene+" img").trigger("click")
+                        },500)
+                    }
 
-					krpano.call('set(hotspot['+hpdata._name+'].ondown, null );');
-					krpano.call('set(hotspot['+hpdata._name+'].onclick,  tween(scale,0.25,0.5); tween(oy,-20,0.5); tween(alpha,0,0.5);js(selectScene('+selectedscene+')) );');
-					$me.find(".hotspotarrow").delay(200).slideUp();
-					$me.find(".removeHotspot").fadeOut();
-					$me.delay(200).animate({
-						width:'330px'
-					})
+                    krpano.call('set(hotspot['+hpdata._name+'].ondown, null );');
+                    krpano.call('set(hotspot['+hpdata._name+'].onclick,  tween(scale,0.25,0.5); tween(oy,-20,0.5); tween(alpha,0,0.5);js(selectScene('+selectedscene+')) );');
+                    $me.find(".hotspotarrow").delay(200).slideUp();
+                    $me.find(".removeHotspot").fadeOut();
+                    $me.delay(200).animate({
+                        width:'330px'
+                    })
 
-				}else{
-					var hpdata = $me.data("spotdata");
-					var krpano = document.getElementById("krpanoSWFObject");
-					krpano.call("addhotspot("+hpdata._name +")");
-					krpano.call('set(hotspot['+hpdata._name+'].ondown, draghotspot() );');
-					krpano.call('set(hotspot['+hpdata._name+'].onclick, js(openHotspotWindowEditor('+hpdata._name+')) );');
-					$me.find(".save-and-close").show();
-					$me.find(".hotspotarrow").delay(200).slideDown();
-					$me.find(".removeHotspot").fadeIn();
-					$me.delay(200).animate({
-						width:oldWidth+'px'
-					})
-				}
-			})
-
-
-			var selectedset = this.model.get("selectedSet");
-			var myid = this.myid;
+                }else{
+                    var hpdata = $me.data("spotdata");
+                    var krpano = document.getElementById("krpanoSWFObject");
+                    krpano.call("addhotspot("+hpdata._name +")");
+                    krpano.call('set(hotspot['+hpdata._name+'].ondown, draghotspot() );');
+                    krpano.call('set(hotspot['+hpdata._name+'].onclick, js(openHotspotWindowEditor('+hpdata._name+')) );');
+                    $me.find(".save-and-close").show();
+                    $me.find(".hotspotarrow").delay(200).slideDown();
+                    $me.find(".removeHotspot").fadeIn();
+                    $me.delay(200).animate({
+                        width:oldWidth+'px'
+                    })
+                }
+            })
 
 
-			var HotSpotDDModel = Backbone.Model.extend({});
-			hotSpotDDModel = new HotSpotDDModel({selectedset:selectedset,kind:"arrow",elemid:myid});
-			var hotSpotsDropDown = new HotSpotsDropDown({model:hotSpotDDModel})
-			hotSpotsDropDown.render();
-			this.autocomplete();
+            var selectedset = this.model.get("selectedSet");
+            var myid = this.myid;
 
 
-			$("#"+me.myid+" .scrollwrapper-scenes").mCustomScrollbar({
-				theme:"minimal-dark",
-				scrollInertia:300
-			});
+            var HotSpotDDModel = Backbone.Model.extend({});
+            hotSpotDDModel = new HotSpotDDModel({selectedset:selectedset,kind:"arrow",elemid:myid});
+            var hotSpotsDropDown = new HotSpotsDropDown({model:hotSpotDDModel})
+            hotSpotsDropDown.render();
+            this.autocomplete();
 
-			$("#"+me.myid+" .dropdown1").click(function (){
-				$("#"+me.myid+" .list-scene").toggleClass('none');
-			})
 
-		},
+            $("#"+me.myid+" .scrollwrapper-scenes").mCustomScrollbar({
+                theme:"minimal-dark",
+                scrollInertia:300
+            });
 
-		removeThis:function(){
-			this.undelegateEvents();
-			$("#"+this.myid).parent(".overlay").remove();
-		},
+            $("#"+me.myid+" .dropdown1").click(function (){
+                $("#"+me.myid+" .list-scene").toggleClass('none');
+            })
 
-		autocomplete: function(){
-				var myid = this.myid;
-				var num = this.myid.replace("spot","");
-				var scenes = tourData.krpano.scene;
-				_.each(scenes,function(elem,ind){
-					elem.label = elem._title;
-			}) 
+        },
 
-			$("#"+myid+" .search-scenes").autocomplete({
-				source:scenes,
-				 appendTo:"#scenes-search-results-"+num,
-				 focus: function( event, ui ) {
-					$("#"+myid+" .search-scenes").val( ui.item._title );
-					return false;
-					}, 
-				select:function(event,ui){
-					var myTitle = ui.item._title;
-					$("#"+myid+" .search-scenes").val(myTitle);
-					/*$("#"+myid+" .inner-modal h2").show();*/
-					$("#"+myid+" .scenes-list #"+ui.item._name).show();
-					return false;
-				},
-				search:function(event,ui){
-				  $("#"+myid+" .scenes-list li").hide();
-					//$("#"+myid+" .inner-modal h2").hide();
-				},
-				change: function(event,ui){
-					if(!ui.item){
-						$("#"+myid+" .search-scenes").val('');
-					}                               
-				}
-			}).data("ui-autocomplete")._renderItem = function(ul,item){
+        removeThis:function(){
+            this.undelegateEvents();
+            $("#"+this.myid).parent(".overlay").remove();
+        },
 
-				return $( "<li></li>" )
-				.data( "item.autocomplete", item )
-				.append( "<img src="+item._thumburl+" /><dl><dt>" + item._title + "</dt><dd>" + item._name + "</dd></dl>" )
-				.appendTo( ul ); 
+        autocomplete: function(){
+                var myid = this.myid;
+                var num = this.myid.replace("spot","");
+                var scenes = tourData.krpano.scene;
+                _.each(scenes,function(elem,ind){
+                    elem.label = elem._title;
+            }) 
 
-			};
+            $("#"+myid+" .search-scenes").autocomplete({
+                source:scenes,
+                 appendTo:"#scenes-search-results-"+num,
+                 focus: function( event, ui ) {
+                    $("#"+myid+" .search-scenes").val( ui.item._title );
+                    return false;
+                    }, 
+                select:function(event,ui){
+                    var myTitle = ui.item._title;
+                    $("#"+myid+" .search-scenes").val(myTitle);
+                    /*$("#"+myid+" .inner-modal h2").show();*/
+                    $("#"+myid+" .scenes-list #"+ui.item._name).show();
+                    return false;
+                },
+                search:function(event,ui){
+                  $("#"+myid+" .scenes-list li").hide();
+                    //$("#"+myid+" .inner-modal h2").hide();
+                },
+                change: function(event,ui){
+                    if(!ui.item){
+                        $("#"+myid+" .search-scenes").val('');
+                    }                               
+                }
+            }).data("ui-autocomplete")._renderItem = function(ul,item){
 
-			$("#"+myid+" .scenes-list li").click(function(e){
-				console.log($(e.target).parent())
-				var name = $(e.target).parent().attr("id");
-				var imgsrc =  $(e.target).parent().find("img").attr("src");
-				console.log(name)
-				$("#"+myid+" h3 .selectedScene").text(name);
-				$("#"+myid+" .list-scene").toggleClass('none');
-				$("#"+myid+" h3.scenedd img").attr("src",imgsrc)
-			})
+                return $( "<li></li>" )
+                .data( "item.autocomplete", item )
+                .append( "<img src="+item._thumburl+" /><dl><dt>" + item._title + "</dt><dd>" + item._name + "</dd></dl>" )
+                .appendTo( ul ); 
 
-			$("#"+myid+" .scenelist .clear-btn").click(function(ev) {                
-				$(ev.target).siblings('input').val('');
-				$("#"+myid+" .scenes-list li").show()
-			})
+            };
 
-		}
+            /* select */
 
-		
-	});
+            $("#"+myid+" .scenes-list li").click(function(e){
+                var name = $(this).attr("id");
+                var panoName = $(this).find('dd').text();
 
-	return ArrowHotspotEditorView;
-	
+                var imgsrc =  $(this).find("img").attr("src");
+
+                $("#"+myid+" h3 .selectedScene").text(name);
+                $("#"+myid+" h3 .selectedFile").text(panoName);
+                $("#"+myid+" .list-scene").toggleClass('none');
+                $("#"+myid+" h3.scenedd img").attr("src",imgsrc)
+            })
+
+            $("#"+myid+" .scenelist .clear-btn").click(function(ev) {                
+                $(ev.target).find('input').val('');
+                $("#"+myid+" .scenes-list li").show()
+            })
+
+        }
+
+        
+    });
+
+    return ArrowHotspotEditorView;
+    
 });
