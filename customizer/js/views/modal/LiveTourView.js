328aabf493202a3835472ea26f0e08171af044b9
hernan <hernan.ainsa@globant.com>
2015-05-05 12:38:15 -0300

    live tour updates

--- customizer/js/views/modal/LiveTourView.js
+++ customizer/js/views/modal/LiveTourView.js
@@ -4,8 +4,9 @@ define([
     'backbone',
     'views/modal/Modal',
     'text!templates/modal/liveTour.html',
+    'helpers/HelpFunctions'
 
-], function($, _, Backbone,Modal,LiveTour){
+], function($, _, Backbone,Modal,LiveTour, HelpFunctions){
 
     var MapModalView = Modal.extend({
         
@@ -24,7 +25,7 @@ define([
 
             //$("#"+myid).find(".save-and-close").unbind("click");
             //$("#"+myid).find(".save-and-close").click(this.doneEdition);
-
+            var helpFunctions = new HelpFunctions();
             var template = _.template(LiveTour);
 
             $("#"+myid+" .inner-modal").html(template);
@@ -33,6 +34,9 @@ define([
             liveTourView.render(myid);*/
 
             this.verticalCent();
+            
+            helpFunctions.dropDown(".dd-liveTour");
+            helpFunctions.checkbox(".check-group","fa-check-square","fa-square");
             /*$(".scrollwrapper").mCustomScrollbar({
                 theme:"minimal-dark",
                 scrollInertia:300
