ea499b5fe4060c14e73b48259e99128bfaa71c23
Maxi <hernanh@Hernan-Hs-Mac-mini.local>
2015-07-02 15:06:41 -0300

    skill

--- customizer/js/views/modal/SimpleControlBtnSkillEditor.js
+++ customizer/js/views/modal/SimpleControlBtnSkillEditor.js
@@ -20,15 +20,62 @@ define([
          _.extend(this.events, Modal.prototype.events);
         },
         events:{
-
+            "click .simple-control-btns-skill-editor .dropdown li":"selectDD"
         },
         
         renderExtend:function(){
 
             var myid = this.myid;
             var tourSkill = this.model.get("tourSkill");
+            switch(tourSkill.skill_controls_settings._position){
+                case "topright":
+                positionlabel = "Top-Right"
+                break;
+                case "topleft":
+                positionlabel = "Top-Left"
+                break;
+                case "top":
+                positionlabel = "Top"
+                break;
+                case "right":
+                positionlabel = "Right"
+                break;
+                case "left":
+                positionlabel = "Left"
+                break;
+                case "bottom":
+                positionlabel = "Bottom"
+                break;
+                case "bottomright":
+                positionlabel = "Bottom-Right"
+                break;
+                case "bottomleft":
+                positionlabel = "Bottom-Left"
+                break;
+            }
 
-            var template = _.template(simpleControlBtnSkillEditor,{tourSkill:tourSkill})
+            var orientationLabel = tourSkill.skill_controls_settings._orientation;
+            orientationLabel = orientationLabel.substring(0,1).toUpperCase()+orientationLabel.substring(1);
+
+            var layoutLabel = tourSkill.skill_controls_settings._layout;
+            layoutLabel = layoutLabel.substring(0,1).toUpperCase()+layoutLabel.substring(1);
+
+            var backimage = tourSkill.skill_controls_settings._icons_styles;
+            backimage = backimage.replace("%SWFPATH%","../player");
+
+            var borderproperties = tourSkill.skill_controls_settings._bgborder.split(" ");
+
+            borderprop = {
+                width: borderproperties[0],
+                color: borderproperties[1],
+                transp: borderproperties[2],
+            }
+            
+            var arrowPos = tourSkill.skill_controls_settings._arrows_position;
+            arrowPos = arrowPos.substring(0,1).toUpperCase()+arrowPos.substring(1);
+
+            console.log(tourSkill)
+            var template = _.template(simpleControlBtnSkillEditor,{tourSkill:tourSkill,positionlabel:positionlabel,orientationLabel:orientationLabel,layoutLabel:layoutLabel,backimage:backimage,borderprop:borderprop,arrowPos:arrowPos})
 
             $("#"+myid+" .inner-modal").html(template);
 
@@ -59,6 +106,17 @@ define([
             });*/
 
         },
+
+
+        selectDD:function(e){
+
+            var myval = $(e.target).data("value");
+            $(e.target).parents(".dropdown").data("selected",myval);
+            var param = $(e.target).parent().data("param");
+            var krpano = document.getElementById("krpanoSWFObject");
+            krpano.call("set(skill_controls_settings."+param+","+myval+"); skill_controls_build();");
+
+        }
 
     });
 
